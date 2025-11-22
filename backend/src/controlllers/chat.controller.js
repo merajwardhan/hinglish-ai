a792ec21f24eae36be09bd04e69a6bcd70760221
imrajwardhan <patilrajwardhan7@gmail.com>
2025-11-22 15:34:49 +0000

    feat: Added the chat controller js file

---
+++ backend/src/controlllers/chat.controller.js
@@ -0,0 +1,17 @@
+import { geminiServices } from '../utils/google.service.js';
+
+async function handleMessage(req, res) {
+  try {
+     const msg = req.body.message;
+     if(msg) return res.status(400).json({ message: `Message content required!`});
+     const response = await geminiServices.sendMessage(msg);
+     res.json({ response }, 200);
+   } catch (error) {
+     console.error(`Something went wrong in the chat controller\nError => ${error}`)
+     res.status(500).json({ error || "An error occured while trying to get back the response"});
+   } 
+}
+
+export default {
+  handleMessage
+}
