63812f0bd98a25de8b9927ea1290b4f18a94c75c
imrajwardhan <patilrajwardhan7@gmail.com>
2025-11-22 15:34:08 +0000

    feat: Added the chat route in the main server js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from 'express';
 const app = express();
 import 'dotenv/config';
+import { chatRouter } from './src/routes/chat.js';
 
 app.get('/', (req,res) => {
   res.send('Hello from express!!');
 })
+
+app.get('/api/chat', chatRouter);
 
 app.listen(`${process.env.PORT}` || 3000, console.log(`App listening on port => ${process.env.PORT}`))
