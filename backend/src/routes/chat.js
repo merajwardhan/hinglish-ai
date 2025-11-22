ce8eabd5a0baaabf1a9207d6a7a901abfc9e0bb0
imrajwardhan <patilrajwardhan7@gmail.com>
2025-11-22 15:35:29 +0000

    feat: Added the chat route in the routers dir

---
+++ backend/src/routes/chat.js
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import ( chatController ) from '../controlllers/chat.controller.js';
+
+const router = Router();
+
+router.post('/', chatController.handleMessage);
+
+export default router;
