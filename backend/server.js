88d5dfaec20589e90cd11e8399322102d94d3f3d
imrajwardhan <patilrajwardhan7@gmail.com>
2025-11-24 14:11:34 +0000

    chore: fixed the chatRouter import and routing mechanism

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 import express from 'express';
 const app = express();
 import 'dotenv/config';
-import { chatRouter } from './src/routes/chat.js';
+import chatRouter from './src/routes/chat.js';
+
+app.use(express.json());
 
 app.get('/', (req,res) => {
   res.send('Hello from express!!');
 })
 
-app.get('/api/chat', chatRouter);
+app.use('/api/chat', chatRouter);
 
 app.listen(`${process.env.PORT}` || 3000, console.log(`App listening on port => ${process.env.PORT}`))
