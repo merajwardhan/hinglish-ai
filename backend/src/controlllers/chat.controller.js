3f1f16887e065ebfbab0f38eda9215c1451582aa
imrajwardhan <patilrajwardhan7@gmail.com>
2025-11-24 14:14:20 +0000

    chore: fixed the error handling logic in the handleMessage function

--- backend/src/controlllers/chat.controller.js
+++ backend/src/controlllers/chat.controller.js
@@ -1,14 +1,14 @@
-import { geminiServices } from '../utils/google.service.js';
+import geminiServices from '../utils/google.service.js';
 
 async function handleMessage(req, res) {
   try {
      const msg = req.body.message;
-     if(msg) return res.status(400).json({ message: `Message content required!`});
+     if(!msg) return res.status(400).json({ message: `Message content required!`});
      const response = await geminiServices.sendMessage(msg);
-     res.json({ response }, 200);
+     res.status(200).json({ response });
    } catch (error) {
      console.error(`Something went wrong in the chat controller\nError => ${error}`)
-     res.status(500).json({ error || "An error occured while trying to get back the response"});
+     res.status(500).json({ error : "An error occured while trying to get back the response"});
    } 
 }
 
