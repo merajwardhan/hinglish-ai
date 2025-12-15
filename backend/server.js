26fbfbcfed6384e6c5daea5c925d6306e751a7aa
imrajwardhan <patilrajwardhan7@gmail.com>
2025-12-15 13:39:07 +0000

    feat: added CORS error handling

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,17 @@ import chatRouter from './src/routes/chat.js';
 
 app.use(express.json());
 
+//CORS handling
+app.use((req,res,next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if(res.method === 'OPTIONS') return res.sendStatus(200);
+
+  next();
+})
+
 app.get('/', (req,res) => {
   res.send('Hello from express!!');
 })
